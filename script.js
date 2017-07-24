fbc425c46cacff7efb54b86e826d059c26f7db93
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-24 13:02:35 +0700

    Rilis1

--- script.js
+++ script.js
@@ -11,11 +11,12 @@ let svg = d3.select('#results')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
+  .style('background', '#cacaca')
 
 // Data reloading
 let reload = () => {
   let GoalsScored = []
-  d3.tsv('stats.tsv', function (err, rows) {
+  d3.tsv('afcw-results.tsv', function (err, rows) {
     if (err) {
       console.log('TSV parse error' + err)
     } else {
@@ -23,14 +24,28 @@ let reload = () => {
         GoalsScored.push(row.GoalsScored)
       })
     }
+    redraw(GoalsScored)
   })
-  redraw(GoalsScored)
 }
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
-
+  debugger
+  svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('class', 'bar')
+  .attr('x', (d, i) => {
+    return i
+  })
+  .attr('y', (d) => {
+    return height - d
+  })
+  .attr('width', width)
+  .attr('height', (d) => {
+    return d
+  })
 }
 
 reload()
