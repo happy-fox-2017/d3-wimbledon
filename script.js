626fdcbf8266c4678fa1034295e8e2b2bf3bfea1
Antoni Angga <antoniangga14@gmail.com>
2017-07-24 14:04:47 +0700

    axes nya udh bisa

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const width = 700,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -12,6 +12,8 @@ let svg = d3.select('#results')
   .attr('width', width)
   .attr('height', height)
   .style('background','#ffffff')
+  .style('padding', '50px')
+  
 
 // Data reloading
 let reload = () => {
@@ -30,11 +32,32 @@ let redraw = (data) => {
   // Your data to graph here
   const yScale = d3.scaleLinear()
   .domain([0,d3.max(data)])
-  .range([0,height])
+  .range([0, height])
+  
+  const yScaleforaxes = d3.scaleLinear()
+  .domain([0,d3.max(data)])
+  .range([height, 0])
+  
   
   const xScale = d3.scaleLinear()
   .domain([0,data.length])
   .range([0,width])
+  
+  var y_axis = d3.axisLeft()
+  .scale(yScaleforaxes)
+  .ticks(4)
+  
+  var x_axis = d3.axisBottom()
+  .scale(xScale)
+  .ticks(46)
+  
+  svg.append("g")
+  .attr("transform", "translate(0,0)")
+  .call(y_axis)
+  
+  svg.append("g")
+  .attr("transform", "translate(0,300)")
+  .call(x_axis)
   
   svg.selectAll('rect')
   .data(data)
@@ -52,22 +75,6 @@ let redraw = (data) => {
     return yScale(d)
   })
   
-  var y_axis = d3.axisLeft()
-  .scale(yScale)
-  .ticks(4)
-  
-  var x_axis = d3.axisBottom()
-  .scale(xScale)
-  .ticks(46)
-  
-  svg.append("g")
-  .attr("transform", "translate(0,0)")
-  .call(y_axis)
-  
-  svg.append("g")
-  .attr("transform", "translate(0,300)")
-  .call(x_axis)
-
 }
 
 reload()
