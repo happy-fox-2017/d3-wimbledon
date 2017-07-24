26d91b3f254cb5a02e82dd4646acf5d2801e5ff3
AndrewSenewe <sindrew.christ@gmail.com>
2017-07-24 14:16:03 +0700

    release 3 updated and done

--- script.js
+++ script.js
@@ -3,18 +3,22 @@
 // Our canvas
 const width = 750,
   height = 300,
-  margin = 20
+  margin = 20,
 marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
   .append('svg')
-  .attr('width', width)
+  .attr('width', width +5)
   .attr('height', height)
+  .style('padding-left', '30')
+  .style('margin-left', marginLeft)
+  .style('padding-bottom', '20')
+  .style('padding-top', '20')
 
 let afc = []
 
-
+// let axis = d3.axisLeft(xScale)
 
 
 
@@ -38,9 +42,20 @@ let redraw = (data) => {
     .domain([0, d3.max(data)])
     .range([0, height])
 
+  let y_Scale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([height, 0])
+
   let xScale = d3.scaleLinear()
     .domain([0, data.length])
     .range([0, width])
+
+  let y_axis = d3.axisLeft(y_Scale).ticks(d3.max(data))
+  let x_axis = d3.axisBottom(xScale).ticks(data.length)
+  const colorScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range(['green', 'blue'])
+
 
   console.log('data', data);
   // Your data to graph here
@@ -59,6 +74,17 @@ let redraw = (data) => {
     .attr('height', (d) => {
       return yScale(d)
     })
+    .attr('fill', colorScale)
+
+  svg.append('g')
+    .attr("transform", "translate(0,300)")
+    .call(x_axis)
+
+
+  svg.append('g')
+    .call(y_axis)
+
+
 }
 
 reload()
