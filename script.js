888e1a794bfceb74942525c9703b234f63147c38
AndrewSenewe <sindrew.christ@gmail.com>
2017-07-24 13:02:45 +0700

    release 2 done

--- script.js
+++ script.js
@@ -12,15 +12,53 @@ let svg = d3.select('#results')
   .attr('width', width)
   .attr('height', height)
 
+let afc = []
+
+
+
+
+
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv("afcw-results.tsv", (data) => {
+    // console.log(data);
+    data.forEach(d => {
+      afc.push(d.GoalsScored)
+    })
+    redraw(afc);
+  })
 }
+
+// reload()
 
 // redraw function
 let redraw = (data) => {
-  // Your data to graph here
+  let yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, height])
 
+  let xScale = d3.scaleLinear()
+    .domain([0, data.length])
+    .range([0, width])
+
+  console.log('data', data);
+  // Your data to graph here
+  svg.selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'goals-chart')
+    .attr('x', (d, i) => {
+      return xScale(i)
+    })
+    .attr('y', (d) => {
+      return 300 - yScale(d)
+    })
+    .attr('width', 15)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
 }
 
 reload()
